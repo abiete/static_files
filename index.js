5e383522d1103bd806f80a579dea2e3d26e2f3da
Luis Pino <lapy0110@gmail.com>
2020-04-14 22:08:01 -0400

    Inicializacion

---
+++ index.js
@@ -0,0 +1,30 @@
+// Imports Necesarios
+const express = require('express');
+const colors = require('colors');
+const morgan = require('morgan');
+
+// Creacion del servidor
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static('public'));
+
+// Settings
+app.set('appName','Abiete Software CDN');
+
+// URLS
+// Para ver las vistas ir a views.js
+
+app.get('/',(req,res)=>{
+    res.end('<h1>Hello, server!</h1>')
+})
+
+// Lanzamiento
+app.listen(3000, () =>{
+    console.log(app.get('appName'));
+    console.log('Developed by Abiete Software')
+    console.log('Server on Port 3000')
+    console.log('')
+})
